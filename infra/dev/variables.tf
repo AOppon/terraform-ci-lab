f60a75629a13aff6dec092c876c0ab96b9b5941a
Andrew Oppon <opponandrew@outlook.com>
2026-04-24 23:53:52 +0100

    add backend to workflow

--- infra/dev/variables.tf
+++ infra/dev/variables.tf
@@ -1,9 +1,9 @@
-# variable "resource_group_name" {
-#   type        = string
-#   description = "Name of the resource group"
-# }
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
+}
 
-# variable "location" {
-#   type        = string
-#   description = "Azure region"
-# }
+variable "location" {
+  type        = string
+  description = "Azure region"
+}
