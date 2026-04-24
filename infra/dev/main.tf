3e7bf41f4b0ecfe627e0d720850ddc502a6b2e55
Andrew Oppon <opponandrew@outlook.com>
2026-04-25 00:15:52 +0100

    update output value with name

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -1,3 +1,4 @@
-resource "null_resource" "example" {}
-
-# This file is intentionally left blank as the main configuration for the development environment.
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
