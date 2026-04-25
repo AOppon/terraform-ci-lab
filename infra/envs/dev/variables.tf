b3aae56133fc45b2937a3469227e976eca3d19f1
Andrew Oppon <opponandrew@outlook.com>
2026-04-26 00:48:34 +0100

    update env vars

--- infra/envs/dev/variables.tf
+++ infra/envs/dev/variables.tf
@@ -1,9 +1,8 @@
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the resource group"
-}
-
 variable "location" {
   type        = string
   description = "Azure region"
+}
+
+variable "environment" {
+  type = string
 }
