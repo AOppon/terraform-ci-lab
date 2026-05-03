27f59df2a2ed5e89ac1b7ed51b30346ec435f0e6
Andrew Oppon <opponandrew@outlook.com>
2026-05-04 00:27:52 +0100

    update

--- infra/envs/dev/versions.tf
+++ infra/envs/dev/versions.tf
@@ -9,5 +9,9 @@ terraform {
   }
 
   backend "azurerm" {
+    resource_group_name  = "tfstate-resource-group"
+    storage_account_name = "tfsstaten907t6"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
   }
 }
